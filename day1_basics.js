7df4443b31005bc3221dafbaab5647ca401f9666
Betelhem-Ashenafi <betelhemafrem@gmail.com>
2025-10-02 11:10:41 +0300

    Add JS basics with variables and object

---
+++ day1_basics.js
@@ -0,0 +1,12 @@
+let name="Betelhem Ashenafi";
+const age=22;
+let isstudent=true;
+
+let student={
+    name: "Betelhem",
+    age: 22,
+    department: "software engineering"};
+
+    console.log(name,age,student);
+    console.log(student);
+
