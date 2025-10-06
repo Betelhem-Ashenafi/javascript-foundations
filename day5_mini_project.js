189e3afc5b08ec7da66e54eae9b77f4a566dc7b4
Betelhem-Ashenafi <betelhemafrem@gmail.com>
2025-10-06 22:11:50 +0300

    Add Week 1 mini project: Number Guessing Game

---
+++ day5_mini_project.js
@@ -0,0 +1,16 @@
+// Number Guessing Game
+const prompt = require('prompt-sync')();
+const secret = Math.floor(Math.random() * 10) + 1;
+let guess = null;
+
+console.log("Guess the secret number (1-10):");
+while (guess !== secret) {
+  guess = Number(prompt("Your guess: "));
+  if (guess > secret) {
+    console.log("Too high!");
+  } else if (guess < secret) {
+    console.log("Too low!");
+  } else {
+    console.log("Correct! You win!");
+  }
+}
